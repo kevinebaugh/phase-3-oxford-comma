75ea563598e024b2403debcae033819ffec5f220
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-01-18 14:24:05 -0500

    commit tree 6e8db7399c510d7bedbbf7a1b6e67f38b381c18e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array[0] + " and " + array[1]
+  else
+    commable = array[0...-1]
+    last = array.last
+    commable.join(", ") + ", and " + last
+  end
 end
